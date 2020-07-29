9727d48270ac737b764af64bed38fe7f41fa73ed
Lissette danis Sanchez hernandez <lisanche@c3r4s6.42madrid.com>
2020-07-29 11:04:45 +0200

    final project

---
+++ get_next_line.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lisanche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/04 17:23:45 by lisanche          #+#    #+#             */
+/*   Updated: 2019/12/20 14:32:02 by lisanche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <stdio.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 9999
+# endif
+
+size_t		ft_strlen(const char *s);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+char		*join_str(char const *s1, char const *s2);
+char		*ft_strdup(const char *s1);
+
+int			has_return(char *str);
+int			get_next_line(int fd, char **line);
+
+#endif
---
+++ get_next_line_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lisanche <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/04 17:49:49 by lisanche          #+#    #+#             */
+/*   Updated: 2020/01/10 17:52:21 by lisanche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+static char	*save_line(char *save)
+{
+	char	*rtn;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	if (!save)
+		return (0);
+	while (save[i] && save[i] != '\n')
+		i++;
+	if (!save[i])
+	{
+		free(save);
+		return (0);
+	}
+	if (!(rtn = malloc(sizeof(char) * ((ft_strlen(save) - i) + 1))))
+		return (0);
+	i++;
+	while (save[i])
+		rtn[j++] = save[i++];
+	rtn[j] = '\0';
+	free(save);
+	return (rtn);
+}
+
+static char	*check_line(char *str)
+{
+	int		i;
+	char	*rtn;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!(rtn = malloc(sizeof(char) * (i + 1))))
+		return (0);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		rtn[i] = str[i];
+		i++;
+	}
+	rtn[i] = '\0';
+	return (rtn);
+}
+
+int			get_next_line(int fd, char **line)
+{
+	char		*buff;
+	char		*save;
+	static char	*array[4096];
+	int			reader;
+
+	save = array[fd];
+	reader = 1;
+	if ((fd < 0 || !line || BUFFER_SIZE <= 0) ||
+	(!(buff = malloc(sizeof(char) * (BUFFER_SIZE + 1)))))
+		return (-1);
+	while (!has_return(save) && reader != 0)
+	{
+		if ((reader = read(fd, buff, BUFFER_SIZE)) == -1)
+		{
+			free(buff);
+			return (-1);
+		}
+		buff[reader] = '\0';
+		save = join_str(save, buff);
+	}
+	free(buff);
+	*line = check_line(save);
+	array[fd] = save_line(save);
+	return (reader == 0 && array[fd] == NULL) ? (0) : (1);
+}
